28704feffe9e5c0e991412d56e096ca2329d1f40
Eddie Garmon <eddie_garmon@hotmail.com>
2016-09-08 14:37:06 -0400

    Move build batch files out of psake directory

--- Build/Psake/clean.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& {Import-Module .\psake.psm1; Invoke-psake .\build_script.ps1 Clean;}" 
-
-Pause
---
+++ Build/build.bat
@@ -0,0 +1,5 @@
+@echo off
+
+powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& {Import-Module .\Psake\psake.psm1; Invoke-psake .\Psake\build_script.ps1 Build;}" 
+
+Pause
