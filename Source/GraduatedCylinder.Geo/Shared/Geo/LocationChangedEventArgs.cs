ee5b0e594506a518fc9b642dcf7080b4945c4658
Sandeep Tanugula <tsandeepkumar@hotmail.com>
2016-09-09 12:19:12 -0400

    Make LocationChangedEventArgs readonly

--- Source/GraduatedCylinder.Geo/Shared/Geo/LocationChangedEventArgs.cs
+++ Source/GraduatedCylinder.Geo/Shared/Geo/LocationChangedEventArgs.cs
@@ -6,13 +6,20 @@ namespace GraduatedCylinder.Geo
 {
     public class LocationChangedEventArgs
     {
+        private readonly GeoPosition _position;
+        private readonly Heading _heading;
+
         public LocationChangedEventArgs(GeoPosition position, Heading heading) {
-            Position = position;
-            Heading = heading;
+            _position = position;
+            _heading = heading;
         }
 
-        public GeoPosition Position { get; set; }
+        public GeoPosition Position {
+            get { return _position; }
+        }
 
-        public Heading Heading { get; set; }
+        public Heading Heading {
+            get { return _heading; }
+        }
     }
 }
