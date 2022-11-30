d06dd9ba704ca65c0173caabab626a9b5fde83e2
Eddie Garmon <eddie_garmon@hotmail.com>
2022-11-30 11:03:01 -0500

    code cleanup

--- Tests/GraduatedCylinder.Geo.Tests/GreatArcSpec.cs
+++ Tests/GraduatedCylinder.Geo.Tests/GreatArcSpec.cs
@@ -1,20 +1,19 @@
 ﻿using DigitalHammer.Testing;
 using Xunit;
 
-namespace GraduatedCylinder.Geo
+namespace GraduatedCylinder.Geo;
+
+public class GreatArcSpec
 {
-    public class GreatArcSpec
-    {
 
-        [Theory]
-        [InlineData(0, 0, 0, 0, 0)]
-        [InlineData(36.081976, -79.967206, 36.103796, -79.934979, 3.778)]
-        [InlineData(34.081976, -89.957206, 36.103796, -79.934979, 938.66)]
-        public void VerifyDistance(double lat1, double long1, double lat2, double long2, double expectedLengthInKilometers) {
-            GeoPosition start = new GeoPosition(lat1, long1);
-            GeoPosition stop = new GeoPosition(lat2, long2);
-            GreatArc.Distance(start, stop).In(LengthUnit.KiloMeter).Value.ShouldBeNear(expectedLengthInKilometers, .0001);
-        }
-
+    [Theory]
+    [InlineData(0, 0, 0, 0, 0)]
+    [InlineData(36.081976, -79.967206, 36.103796, -79.934979, 3.778)]
+    [InlineData(34.081976, -89.957206, 36.103796, -79.934979, 938.66)]
+    public void VerifyDistance(double lat1, double long1, double lat2, double long2, double expectedLengthInKilometers) {
+        GeoPosition start = new(lat1, long1);
+        GeoPosition stop = new(lat2, long2);
+        GreatArc.Distance(start, stop).In(LengthUnit.KiloMeter).Value.ShouldBeNear(expectedLengthInKilometers, .0001);
     }
+
 }
